0b11cf49497b5f97a86dbd78329b8164cec8b18f
Sergey Slotin <me@sereja.me>
2022-01-01 20:03:37 +0300

    prefetching in branchless binary search

---
+++ binsearch/branchless-prefetch.cc
@@ -0,0 +1,21 @@
+#include "binsearch.hh"
+
+int n;
+int *t;
+
+void prepare(int *a, int _n) {
+    t = new int[n = _n];
+    memcpy(t, a, 4 * n);
+}
+
+int lower_bound(int x) {
+    int *base = t, len = n;
+    while (len > 1) {
+        int half = len / 2;
+        __builtin_prefetch(&base[(len - half) / 2]);
+        __builtin_prefetch(&base[half + (len - half) / 2]);
+        base = (base[half] < x ? &base[half] : base);
+        len -= half;
+    }
+    return *(base + (*base < x));
+}
