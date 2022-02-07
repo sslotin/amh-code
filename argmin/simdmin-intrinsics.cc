79877ee80b6963ad54d954a2c6a545ed9b7a0119
Sergey Slotin <me@sereja.me>
2022-02-07 08:55:35 +0300

    simdmin with intrinsics

--- argmin/simdmin-intrinsics.cc
+++ argmin/simdmin-intrinsics.cc
@@ -6,19 +6,20 @@ typedef __m256i reg;
 int argmin() {
     int min = INT_MAX, idx = 0;
     
-    reg p = _mm256_set1_epi32(m);
+    reg p = _mm256_set1_epi32(min);
 
     for (int i = 0; i < n; i += 8) {
-        reg t = _mm256_min_epi32(t, (reg*) &a[i]);
-        
-        int mask = _mm256_movemask_epi8((__m256i) (p == t));
-        if (mask != -1) { [[unlikely]]
-            for (int j = B * i; j < B * i + 2 * B; j++)
-                if (a[j] < m)
-                    m = a[k = j];
-            t = p = m + vec{};
+        reg y = _mm256_load_si256((reg*) &a[i]); 
+        reg t = _mm256_min_epi32(t, y);
+        reg mask = _mm256_cmpeq_epi32(t, p);
+        int m = _mm256_movemask_epi8(mask);
+        if (m != -1) { [[unlikely]]
+            for (int j = i; j < i + 8; j++)
+                if (a[j] < min)
+                    min = a[idx = j];
+            t = p = _mm256_set1_epi32(min);
         }
     }
     
-    return k;
+    return idx;
 }
