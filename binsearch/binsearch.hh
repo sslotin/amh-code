95f0a33dae5fb8f92e2dee7f5c8bb0b61f0cb4e9
Sergey Slotin <me@sereja.me>
2022-02-11 12:57:27 +0300

    measuring actual binary search latency

--- binsearch/binsearch.hh
+++ binsearch/binsearch.hh
@@ -14,14 +14,11 @@ int main(int argc, char* argv[]) {
     int *a = new int[n];
     int *q = new int[m];
 
-    //for (int i = 0; i < n; i++)
-    //    a[i] = i;
-
     for (int i = 0; i < n; i++)
         a[i] = rand();
     for (int i = 0; i < m; i++)
-        q[i] = rand()/* % n*/;
-    
+        q[i] = rand();
+
     a[0] = RAND_MAX;
     std::sort(a, a + n);
 
@@ -30,27 +27,20 @@ int main(int argc, char* argv[]) {
     int checksum = 0;
     clock_t start = clock();
 
-    /*
-    for (int i = 0; i < m; i++) {
-        int x = lower_bound(q[i]);
-        int y = *std::lower_bound(a, a + n, q[i]);
-        if (x != y) {
-            std::cout << q[i] << " " << x << " " << y << std::endl;
-            //for (int j = 0; j < n; j++)
-            //    if (abs(a[j] - q[i]) <= 2)
-            //        std::cout << a[j] << std::endl;
-            return 0;
-        }
-    }
-    */
-    
+    #ifdef LATENCY
+    int last = 0;
 
+    for (int i = 0; i < m; i++) {
+        last = lower_bound(q[i] ^ last);
+        checksum ^= last;
+    }
+    #else
     for (int i = 0; i < m; i++)
         checksum ^= lower_bound(q[i]);
+    #endif
 
     float seconds = float(clock() - start) / CLOCKS_PER_SEC;
 
-    //printf("%.4f s total time\n", seconds);
     printf("%.2f ns per query\n", 1e9 * seconds / m);
     printf("%d\n", checksum);
     
