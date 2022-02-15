f7b71d85129885b3e32baf36eee505581c063e7a
Sergey Slotin <me@sereja.me>
2022-02-15 18:36:57 +0300

    self-contained binary search benchmark

--- binsearch/selfcontained.cc
+++ binsearch/selfcontained.cc
@@ -1,139 +1,81 @@
-#pragma GCC optimize("O3")
-#pragma GCC target("avx2")
- 
-#include <x86intrin.h>
-#include <bits/stdc++.h>
- 
-using namespace std;
- 
-const int n = (1<<22), m = (1<<23);
+#include <ctime>
+#include <cstdio>
+#include <algorithm>
+
+// play with the array size (n) and compiler versions
+const int n = 1e6, m = 2e6;
+
 int a[n], q[m], results[m];
-alignas(64) int b[n+1], c[n];
- 
-const int block_size = 16;
-const int B = block_size;
-const int INF = numeric_limits<int>::max();
- 
-int eytzinger(int i = 0, int k = 1) {
+alignas(64) int t[n + 1];
+
+void build(int k = 1) {
+    static int i = 0;
     if (k <= n) {
-        i = eytzinger(i, 2 * k);
-        b[k] = a[i++];
-        i = eytzinger(i, 2 * k + 1);
+        build(2 * k);
+        t[k] = a[i++];
+        build(2 * k + 1);
     }
-    return i;
 }
- 
-int search(int x) {
+
+int baseline(int x) {
+    return *std::lower_bound(a, a + n, x);
+}
+
+int branchless(int x) {
+    int *base = a, len = n;
+    while (len > 1) {
+        int half = len / 2;
+        base = (base[half] < x ? &base[half] : base);
+        len -= half;
+    }
+    return *(base + (*base < x));
+}
+
+int eytzinger(int x) {
     int k = 1;
     while (k <= n) {
-        __builtin_prefetch(b + k * block_size);
-        k = 2 * k + (b[k] < x);
+        __builtin_prefetch(t + k * 16);
+        k = 2 * k + (t[k] < x);
     }
     k >>= __builtin_ffs(~k);
-    return k;
+    return t[k];
 }
- 
-const int nblocks = (n + B - 1) / B;
-alignas(64) int btree[nblocks][B];
- 
-int go(int k, int i) { return k * (B + 1) + i + 1; }
- 
-void build(int k = 0) {
-    static int t = 0;
-    if (k < nblocks) {
-        for (int i = 0; i < B; i++) {
-            build(go(k, i));
-            btree[k][i] = (t < n ? a[t++] : INF);
-        }
-        build(go(k, B));
-    }
-}
- 
-int bsearch(int x) {
-    int k = 0, res = INF;
-    start:
-    while (k < nblocks) {
-        for (int i = 0; i < B; i++) {
-            if (btree[k][i] >= x) {
-                res = btree[k][i];
-                k = go(k, i);
-                goto start;
-            }
-        }
-        k = go(k, B);
-    }
-    return res;
-}
- 
-typedef __m256i reg;
- 
-int cmp(reg x_vec, int* y_ptr) {
-    reg y_vec = _mm256_load_si256((reg*) y_ptr);
-    reg mask = _mm256_cmpgt_epi32(x_vec, y_vec);
-    return _mm256_movemask_ps((__m256) mask);
-}
- 
-int bsearch_avx2(int x) {
-    int k = 0, res = INF;
-    reg x_vec = _mm256_set1_epi32(x);
-    while (k < nblocks) {
-        int mask = ~(
-            cmp(x_vec, &btree[k][0]) +
-            (cmp(x_vec, &btree[k][8]) << 8)
-        );
-        int i = __builtin_ffs(mask) - 1;
-        if (i < B)
-            res = btree[k][i];
-        k = go(k, i);
-    }
-    return res;
-}
- 
-template<typename lambda>
-double timeit(string name, lambda f) {
-    cout << "> " << name << endl;
+
+float timeit(int (*lower_bound)(int)) {
     clock_t start = clock();
-    f();
-    double t = double(clock() - start) / CLOCKS_PER_SEC;
-    cout << "time: " << t << endl;
-    return t;
+
+    int checksum = 0;
+
+    for (int i = 0; i < m; i++)
+        checksum += lower_bound(q[i]);
+    
+    float duration = float(clock() - start) / CLOCKS_PER_SEC;
+
+    printf("  checksum: %d\n", checksum);
+    printf("  latency: %.2fns\n", 1e9 * duration / m);
+
+    return duration;
 }
- 
+
 int main() {
     for (int i = 0; i < n; i++)
-        a[i] = (rand() << 16) + rand();
+        a[i] = rand(); // <- careful on 16-bit platforms
     for (int i = 0; i < m; i++)
-        q[i] = (rand() << 16) + rand();
-    a[0] = INF;
- 
-    sort(a, a+n);
-    eytzinger();
-    build();
- 
-    double t1 = timeit("std::lower_bound", [&](){
-        for (int i = 0; i < m; i++)
-            results[i] = *lower_bound(a, a + n, q[i]);
-    });
- 
-    timeit("eytzinger", [&](){
-        for (int i = 0; i < m; i++) {
-            assert(results[i] == b[search(q[i])]);
-        }
-    });
- 
-    timeit("btree", [&](){
-        for (int i = 0; i < m; i++) {
-            assert(results[i] == bsearch(q[i]));
-        }
-    });
- 
-    double t2 = timeit("btree-avx2", [&](){
-        for (int i = 0; i < m; i++) {
-            assert(results[i] == bsearch_avx2(q[i]));
-        }
-    });
+        q[i] = rand();
     
-    cout << "spedup: " << t1 / t2 << endl;
+    a[0] = RAND_MAX; // to avoid dealing with end-of-array iterators 
+ 
+    std::sort(a, a + n);
+    build();
+
+    printf("std::lower_bound:\n");
+    float x = timeit(baseline);
+    
+    printf("branchless:\n");
+    printf("  speedup: %.2fx\n", x / timeit(branchless));
+ 
+    printf("eytzinger:\n");
+    printf("  speedup: %.2fx\n", x / timeit(eytzinger));
  
     return 0;
 }
