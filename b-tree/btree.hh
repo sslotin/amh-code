e11cc9bb7f0a9096b9e30830176727cc2f677433
Sergey Slotin <me@sereja.me>
2022-03-24 22:06:00 +0300

    benchmarking of small tree sizes

--- b-tree/btree.hh
+++ b-tree/btree.hh
@@ -21,6 +21,27 @@ int main() {
 
     volatile int checksum = 0;
     
+    /*
+    int size = 0;
+    for (int S = 1; S <= L; S += 10) {
+        // benchmark insertions
+        for (int i = 0; i < 10; i++)
+            insert(x[size++]);
+
+        for (int i = 0; i < Q; i++)
+            y[i] = rand() % LIM;
+
+        // benchmark reads
+        clock_t start = clock();
+
+        for (int i = 0; i < Q; i++)
+            checksum ^= lower_bound(y[i]);
+
+        float duration = float(clock() - start) / CLOCKS_PER_SEC;
+        printf("%d %.2f\n", size, 1e9 * duration / Q);
+    }
+    */
+
     int size = 0;
     for (int S = L; S < R; S *= K) {
         // benchmark insertions
