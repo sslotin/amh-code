96a502ef595446b020d263a883cd11e996b0a86f
Sergey Slotin <me@sereja.me>
2022-03-31 16:48:19 +0300

    transpose over an array

--- parsing/transpose-array.cc
+++ parsing/transpose-array.cc
@@ -8,6 +8,56 @@ const int P = SIZE / K / 8;   // partition size
 
 typedef __m256i reg;
 
+void transpose_int(int *a, int *b) {
+    // todo: fix indices
+    for (int i = 0; i < P; i += 8) {
+        reg r1 = _mm256_load_si256((reg*) &a[0 * P + i]);
+        reg r3 = _mm256_load_si256((reg*) &a[1 * P + i]);
+        reg r2 = _mm256_load_si256((reg*) &a[2 * P + i]);
+        reg r4 = _mm256_load_si256((reg*) &a[3 * P + i]);
+        reg r5 = _mm256_load_si256((reg*) &a[4 * P + i]);
+        reg r7 = _mm256_load_si256((reg*) &a[5 * P + i]);
+        reg r6 = _mm256_load_si256((reg*) &a[6 * P + i]);
+        reg r8 = _mm256_load_si256((reg*) &a[7 * P + i]);
+
+        reg d1 = _mm256_unpacklo_epi32(r1, r2);
+        reg d2 = _mm256_unpackhi_epi32(r1, r2);
+        reg d3 = _mm256_unpacklo_epi32(r3, r4);
+        reg d4 = _mm256_unpackhi_epi32(r3, r4);
+        reg d5 = _mm256_unpacklo_epi32(r5, r6);
+        reg d6 = _mm256_unpackhi_epi32(r5, r6);
+        reg d7 = _mm256_unpacklo_epi32(r7, r8);
+        reg d8 = _mm256_unpackhi_epi32(r7, r8);
+
+        reg q1 = _mm256_unpacklo_epi32(d1, d3);
+        reg q2 = _mm256_unpackhi_epi32(d1, d3);
+        reg q3 = _mm256_unpacklo_epi32(d2, d4);
+        reg q4 = _mm256_unpackhi_epi32(d2, d4);
+        reg q5 = _mm256_unpacklo_epi32(d5, d7);
+        reg q6 = _mm256_unpackhi_epi32(d5, d7);
+        reg q7 = _mm256_unpacklo_epi32(d6, d8);
+        reg q8 = _mm256_unpackhi_epi32(d6, d8);
+
+        r1 = _mm256_permute2f128_si256(q1, q5, 0x20);
+        r2 = _mm256_permute2f128_si256(q2, q6, 0x20);
+        r3 = _mm256_permute2f128_si256(q3, q7, 0x20);
+        r4 = _mm256_permute2f128_si256(q4, q8, 0x20);
+        r5 = _mm256_permute2f128_si256(q1, q5, 0x31);
+        r6 = _mm256_permute2f128_si256(q2, q6, 0x31);
+        r7 = _mm256_permute2f128_si256(q3, q7, 0x31);
+        r8 = _mm256_permute2f128_si256(q4, q8, 0x31);
+
+        _mm256_store_si256((reg*) &b[8 * i + 0], r1);
+        _mm256_store_si256((reg*) &b[8 * i + 8], r2);
+        _mm256_store_si256((reg*) &b[8 * i + 16], r3);
+        _mm256_store_si256((reg*) &b[8 * i + 24], r4);
+        _mm256_store_si256((reg*) &b[8 * i + 32], r5);
+        _mm256_store_si256((reg*) &b[8 * i + 40], r6);
+        _mm256_store_si256((reg*) &b[8 * i + 48], r7);
+        _mm256_store_si256((reg*) &b[8 * i + 56], r8);
+    }
+}
+
 void transpose(char *a, char *b) {
     for (int i = 0; i < P; i += 32) {
         reg r1 = _mm256_load_si256((reg*) &a[0 * P + i]);
@@ -58,6 +108,7 @@ void transpose(char *a, char *b) {
 }
 
 alignas(64) char a[SIZE], b[SIZE];
+alignas(64) int res[SIZE], tra[SIZE];
 int buf_len = 0, buf_pos = 0;
 
 void scalar_parse(int &s) {
@@ -97,11 +148,11 @@ int parse_int(char *s) {
 // parses a full block
 // returns remaining bytes
 int simd_parse(int &_s) {
-    reg s[K], x[K];
+    reg x[K];
 
     for (int i = 0; i < K; i++) {
         transpose(a + i * (SIZE / K), b + i * (SIZE / K));
-        s[i] = x[i] = _mm256_setzero_si256();
+        x[i] = _mm256_setzero_si256();
     }
 
     for (int i = 0; i < SIZE / K; i += 256) {
@@ -113,8 +164,10 @@ int simd_parse(int &_s) {
                         const reg real_zero = _mm256_setzero_si256();
                         const reg ten = _mm256_set1_epi32(10);
 
+                        int idx = p * (SIZE / K) + i + j * 16 + k * 32 + l * 8;
+
                         reg c = _mm256_cvtepu8_epi32(
-                            _mm_loadl_epi64((__m128i*) &b[p * (SIZE / K) + i + j * 16 + k * 32 + l * 8])
+                            _mm_loadl_epi64((__m128i*) &b[idx])
                         );
 
                         reg mask = _mm256_cmpgt_epi32(zero, c); // 1s are separators
@@ -122,7 +175,9 @@ int simd_parse(int &_s) {
                         c = _mm256_sub_epi32(c, zero);
 
                         reg y = _mm256_add_epi32(_mm256_mullo_epi32(x[p], ten), c);
-                        s[p] = _mm256_xor_si256(s[p], _mm256_and_si256(x[p], mask));
+                        
+                        //s[p] = _mm256_xor_si256(s[p], _mm256_and_si256(x[p], mask));
+                        _mm256_store_si256((reg*) &res[idx], _mm256_and_si256(x[p], mask));
 
                         x[p] = _mm256_blendv_epi8(y, real_zero, mask);
                     }
@@ -131,11 +186,45 @@ int simd_parse(int &_s) {
         }
     }
 
-    for (int i = 1; i < K; i++)
-        s[0] = _mm256_xor_si256(s[0], s[i]);
+    /*
+    for (int p = 0; p < K; p++) {
+        for (int q = 0; q < 8; q++) {
+            for (int i = 0; i < SIZE / K; i += 256) {
+                for (int j = 0; j < 2; j++) {
+                    for (int k = 0; k < 8; k++) {
+                        for (int l = 0; l < 2; l++) {
+                            int idx = p * (SIZE / K) + q + i + j * 16 + k * 32 + l * 8;
+                            _s ^= res[idx];
+                            //if (res[idx])
+                            //    printf("%d %d\n", idx, res[idx]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    */
+
+    for (int i = 0; i < K; i++)
+        transpose_int(res + i * (SIZE / K), tra + i * (SIZE / K));
+
+    for (int i = 0; i < 100; i++) {
+        if (tra[i])
+            printf("%d %d\n", i, tra[i]);
+    }
+
+    exit(0);
+
+    reg s = _mm256_setzero_si256();
+
+    for (int i = 0; i < SIZE; i += 8) {
+        reg x = _mm256_load_si256((reg*) &tra[i]);
+        s = _mm256_xor_si256(s, x);
+        //_s ^= res[i];
+    }
 
     int buf[8];
-    _mm256_storeu_si256((reg*) buf, s[0]);
+    _mm256_storeu_si256((reg*) buf, s);
 
     for (int i = 0; i < 8; i++)
         _s ^= buf[i];
