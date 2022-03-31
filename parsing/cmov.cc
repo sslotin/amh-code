540fd983864c37103bf0942d65ea80e752b829b8
Sergey Slotin <me@sereja.me>
2022-03-31 15:08:02 +0300

    cmov-based parsing

---
+++ parsing/cmov.cc
@@ -0,0 +1,36 @@
+#include "read.hh"
+
+void prepare() {}
+
+int read(int n) {
+    const int SIZE = (1<<14); // 16K
+    char buf[SIZE];
+
+    int s = 0, x = 0;
+
+    while (true) {
+        int buf_len = fread(buf, 1, SIZE, stdin);
+        int k = 0;
+
+        if (buf_len != SIZE)
+            break;
+
+        while (k < SIZE) {
+            char c = buf[k++];
+            
+            bool cond = (c == '\n');
+
+            s ^= (cond ? x : 0);
+
+            x = 10 * x + (c - '0');
+            x = (cond ? 0 : x);
+
+            //n -= cond;
+
+            //if (n == 0)
+            //    return s;
+        }
+    }
+
+    return s; // never reached
+}
