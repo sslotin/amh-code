fb9ed6a6b1f2f3da4f55beb8aa121dcd6035aef7
Sergey Slotin <me@sereja.me>
2022-05-18 08:22:41 +0300

    montgomery implementations

--- inverse/montgomery.cc
+++ inverse/montgomery.cc
@@ -1,9 +1,52 @@
 #include "inverse.hh"
+#include <x86intrin.h>
 
 typedef __uint32_t u32;
 typedef __uint64_t u64;
 typedef __uint128_t u128;
 
+typedef __m256i v8i;
+
+struct montgomery {
+    u32 n, nr, r2;
+    
+    constexpr montgomery(u32 n) : n(n), nr(1), r2((u128(1) << 64) % n) {
+        for (int i = 0; i < 6; i++)
+            nr *= 2 - n * nr;
+    }
+
+    /*
+    u32 reduce(u64 x) const {
+        u32 q = u32(x) * nr;
+        u64 m = (u64) q * n;
+        u32 y = ((x - m) >> 32);
+        return x < m ? y + n : y;
+    }
+    */
+
+    u32 reduce(u64 x) const {
+        u32 q = u32(x) * nr;
+        u32 m = ((u64) q * n) >> 32;
+        //std::cout << xhi << " " << (xhi >= m) << std::endl;
+        return (x >> 32) + n - m;
+        //u32 t = xhi - m;
+        //return xhi >= m ? t : t + n;
+    }
+
+    //v8i multiply(v8i x, v8i y) const {   
+    //    v8i = _mm256_mullo_epi32
+    //}
+
+    u32 multiply(u32 x, u32 y) const {
+        return reduce((u64) x * y);
+    }
+
+    u32 transform(u32 x) const {
+        return reduce(u64(x) << 32);
+    }
+};
+
+/*
 struct montgomery {
     u64 n, nr;
     
@@ -16,17 +59,17 @@ struct montgomery {
         u64 q = u64(x) * nr;
         u64 m = ((u128) q * n) >> 64;
         u64 xhi = (x >> 64);
-        u64 t = xhi - m;
-        return xhi >= m ? t : t + n;
-        /*
-        if (xhi >= m)
-            return (xhi - m);
-        else
-            return (xhi - m) + n;
-        */
+        //std::cout << xhi << " " << (xhi >= m) << std::endl;
+        return xhi + n - m;
+        //u32 t = xhi - m;
+        //return xhi >= m ? t : t + n;
     }
 
-    u64 mult(u64 x, u64 y) const {
+    //v8i multiply(v8i x, v8i y) const {   
+    //    v8i = _mm256_mullo_epi32
+    //}
+
+    u64 multiply(u64 x, u64 y) const {
         return reduce((u128) x * y);
     }
 
@@ -34,34 +77,37 @@ struct montgomery {
         return (u128(x) << 64) % n;
     }
 };
+*/
 
-constexpr montgomery space(m);
+constexpr montgomery space(M);
 
+/*
 int inverse(int _a) {
-    //u64 a = _a, r = 1;
-    u64 a = space.transform(_a);
-    u64 r = space.transform(1);
-    int n = m - 2;
+    u32 a = space.transform(_a);
+    u32 r = space.transform(1);
+    
+    int n = M - 2;
     while (n) {
         if (n & 1)
-            r = space.mult(r, a);
-        a = space.mult(a, a);
+            r = space.multiply(r, a);
+        a = space.multiply(a, a);
         n >>= 1;
     }
-    //cout << _a << " " << r << " " << r * _a % m << endl;
+    
     return space.reduce(r);
 }
-
+*/
 
 int inverse(int _a) {
-    long long a = _a, r = 1;
+    u32 a = space.transform(_a);
+    u32 r = space.transform(1);
     
     #pragma GCC unroll(30)
     for (int l = 0; l < 30; l++) {
         if ( (M - 2) >> l & 1 )
-            r = r * a % M;
-        a = a * a % M;
+            r = space.multiply(r, a);
+        a = space.multiply(a, a);
     }
 
-    return r;
+    return space.reduce(r);
 }
