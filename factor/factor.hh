f0ad32b404f0049c4268b0a5481067f688b169ef
Sergey Slotin <me@sereja.me>
2022-05-24 16:47:20 +0300

    counting factorization errors

--- factor/factor.hh
+++ factor/factor.hh
@@ -9,19 +9,21 @@ typedef __uint128_t u128;
 u64 find_factor(u64 n);
 
 int main() {
-    int cnt = 0;
+    int cnt = 0, err = 0;
     clock_t start = clock();
 
     u64 n;
     while (cin >> n) {
         u64 d = find_factor(n);
-        assert(d != 1 && d != n && n / d * d == n);
+        err += !(d != 1 && d != n && n / d * d == n);
         cnt++;
     }
 
+    assert(cnt > err * 100);
+
     float duration = float(clock() - start) / CLOCKS_PER_SEC;
 
-    printf("%f\n", cnt / duration);
+    printf("%f %d/%d\n", cnt / duration, err, cnt);
 
     return 0;
 }
