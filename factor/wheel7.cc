7e3ab37d5313d2225ca7c5d74c9b08419f52898e
Sergey Slotin <me@sereja.me>
2022-05-25 15:57:08 +0300

    refactor wheel factorization

--- factor/wheel7.cc
+++ factor/wheel7.cc
@@ -4,10 +4,10 @@ u64 find_factor(u64 n) {
     for (u64 d : {2, 3, 5})
         if (n % d == 0)
             return d;
-    u64 increments[] = {0, 4, 6, 10, 12, 16, 22, 24};
+    u64 offsets[] = {0, 4, 6, 10, 12, 16, 22, 24};
     for (u64 d = 7; d * d <= n; d += 30) {
-        for (u64 k = 0; k < 8; k++) {
-            u64 x = d + increments[k];
+        for (u64 offset : offsets) {
+            u64 x = d + offset;
             if (n % x == 0)
                 return x;
         }
