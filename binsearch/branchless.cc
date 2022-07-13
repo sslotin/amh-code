1b567670f63a2b9f38790c0da93fb6ab5f96c07d
Sergey Slotin <me@sereja.me>
2022-07-13 17:56:22 +0300

    branchless binary search variants

--- binsearch/branchless.cc
+++ binsearch/branchless.cc
@@ -8,6 +8,7 @@ void prepare(int *a, int _n) {
     memcpy(t, a, 4 * n);
 }
 
+/*
 int lower_bound(int x) {
     int *base = t, len = n;
     while (len > 1) {
@@ -17,3 +18,45 @@ int lower_bound(int x) {
     }
     return *(base + (*base < x));
 }
+*/
+
+/*
+int lower_bound(int x) {
+    int *base = t, len = n;
+    while (len > 1) {
+        int half = len / 2;
+        base += (base[half - (~len & 1)] < x) * half;
+        len -= half;
+    }
+    return *base;
+}
+*/
+
+int lower_bound(int x) {
+    int *base = t, len = n;
+    while (len > 1) {
+        base += (base[(len - 1) / 2] < x) * (len / 2);
+        len = (len + 1) / 2;
+    }
+    return *base;
+}
+
+/*
+int lower_bound(int x) {
+    int *base = t, len = n;
+    while (len > 1) {
+        //int half = len / 2;
+        //int half1 = (len - 1) / 2;
+        //base += (base[half1] < x ? half : 0);
+        //len -= half;
+        
+        int half = (len - 1) >> 1;
+        int new_len = (len + 1) >> 1;
+        base += (base[half] < x) * (len - new_len); //new_base + new_len = len
+        len = new_len;
+
+        //std::cout << len << std::endl;
+    }
+    return *base;
+}
+*/
